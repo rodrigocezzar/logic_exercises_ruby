d3cf1ffdb96db77575e248a464d1bac9cfd49718
rodrigo_abreu <rodrigocezzar85@gmail.com>
2023-10-21 13:43:34 -0300

    fix/refatoring bin_to_dec method

--- exercises/bin_to_dec.rb
+++ exercises/bin_to_dec.rb
@@ -2,12 +2,8 @@
 
 class BinToDec
   def self.convert(number)
-    decimal = 0
-    base = 1
-    number.to_s.reverse.each_char do |digito|
-      decimal += digito.to_i * base
-      base *= 2
-    end
-    decimal
+    # your code here \/ \/ \/
+
+    # your code here /\ /\ /\
   end
 end
