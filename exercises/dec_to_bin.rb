7a0c52df0406d5fd5757ef9f75ce99a984dbc11c
rodrigo_abreu <rodrigocezzar85@gmail.com>
2023-10-21 13:49:03 -0300

    fix/dec_to_bin method

--- exercises/dec_to_bin.rb
+++ exercises/dec_to_bin.rb
@@ -2,11 +2,8 @@
 
 class DecToBin
   def self.convert(number)
-    binary = []
-    while number > 0
-      binary.unshift(number % 2)
-      number = number / 2
-    end
-    binary.join.to_i
+    # your code here \/ \/ \/
+
+    # your code here /\ /\ /\
   end
 end
